5a5a5708a564e6bafd35d566d4636eddb25aca00
Matthew Speers <97178743+speers26@users.noreply.github.com>
2024-05-02 18:06:59 +0100

    Update dens_est.R

--- dens_est.R
+++ dens_est.R
@@ -1,13 +1,10 @@
-setwd("~/GitHub/environment-modelling")
-
 library(cond.extremes)
-source("~/GitHub/env-contours/FORM_functions_revised.R")
+source("~FORM_functions.R")
 
 ## read in data
-data <- read.csv("data/cnsTS.txt")
+data <- read.csv("cnsTS.txt")
 
 ## isolate peaks
-
 hs_peak <- c()
 t2_peak <- c()
 for (i in 1:max(data$StrIdn)){
@@ -26,6 +23,4 @@ dens <- grid.dens(data = matrix(c(hs_peak, stp_peak), ncol = 2),
                   ylim = c(0.01, 0.08), nx = 180, ny = 90, log = T, adjust = 0.2)
 print(sum(dens$p))
 
-write.csv(dens, "data/env_probs.csv", row.names = FALSE)
-write.csv(dens, "~/GitHub/force_calculation_and_wave_sim/scripts/response_sim/env_probs.csv",
-          row.names = FALSE)
+write.csv(dens, "env_probs.csv", row.names = FALSE)
