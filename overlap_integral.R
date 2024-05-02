1f45c2b35553d48c9816370d9d8cfefc1906798b
Matthew Speers <97178743+speers26@users.noreply.github.com>
2024-05-03 00:10:18 +0100

    Add files via upload

---
+++ overlap_integral.R
@@ -0,0 +1,47 @@
+setwd("~/GitHub/env-contours")
+library("pracma")
+
+# get contours  -----------------------------------------------------------
+fits =  c("neggev_exp_exp", "negweibull_exp_exp", "neglnorm_lin_lin", "neggamma_qua_lin",
+          "posgev_exp_qua", "posweibull_lin_qua", "poslnorm_qua_exp", "posgamma_lin_lin")
+
+file_names = paste("FORM fits/",sapply((sapply(fits, list.files, path="FORM fits", simplify=FALSE)), '[[', 1), sep="")
+
+for (i in 1:length(fits)){
+  load(file_names[i])
+  if (exists("form")){
+    assign(fits[i], form)
+    rm(form)
+  }
+  if (exists("fit")){
+    assign(fits[i], fit)
+    rm(fit)
+  }
+}
+
+
+# load cond_density -------------------------------------------------------
+
+cond_dens = read.csv("~/GitHub/environment-modelling/data/cond_dens_pos.csv")
+dens = read.csv("~/GitHub/environment-modelling/data/env_probs.csv")
+cond_dens = cond_dens/sum(cond_dens)
+
+# calculate integral ------------------------------------------------------
+
+overlap_int = function(cntr, cell_x, cell_y, dens, plot=F){
+  if (plot){
+    plot(cell_x, cell_y)
+    mask = inpolygon(cell_x, cell_y, cntr$x, cntr$y) 
+    points(cell_x[mask], cell_y[mask], col="yellow")
+  }
+  sum(inpolygon(cell_x, cell_y, cntr$x, cntr$y) * dens)
+}
+
+contour_data = sapply(fits, get)
+overlap_ps = lapply(contour_data, overlap_int, cell_x=dens$x, cell_y=dens$y, dens=cond_dens)
+
+metric = function(p){
+  2 * p -1
+}
+
+lapply(overlap_ps, metric)
---
+++ predictive_llh_cross_validation.R
@@ -0,0 +1,55 @@
+################################################################################
+##### Script for validating density fits using predictive tail likelihood ######
+################################################################################
+
+# functions ---------------------------------------------------------------
+
+get_group = function(data, inds, j){
+  data[inds==j]
+}
+
+split_training_test = function(data, k, threshold_q){
+  #' Function that splits sample into training and test sets, ready for k-fold
+  #' cross validation on the extreme portion of the data
+  #' @param data matrix of dataset x and y
+  #' @param k number of folds
+  #' @param threshold_q extreme threshold quantile
+  
+  u = quantile(data[,1], threshold_q, names=F)
+  x_lower = data[,1][data[,1]<=u]; y_lower = data[,2][data[,1]<=u]
+  x_upper = data[,1][data[,1]>u]; y_upper = data[,2][data[,1]>u]
+  
+  group_ind = rep(1:k, ceiling(length(x_upper)/k))[1:length(x_upper)]
+  x_splits = sapply(1:k, get_group, data=x_upper, inds=group_ind)
+  y_splits = sapply(1:k, get_group, data=y_upper, inds=group_ind)
+  
+  folds = list()
+  for (i in 1:k){
+    training_x = c(x_lower, unlist(x_splits[-i]))
+    training_y = c(y_lower, unlist(y_splits[-i]))
+    testing_x = unlist(x_splits[i])
+    testing_y = unlist(y_splits[i])  
+    
+    folds[[i]] = list(train=matrix(c(training_x, training_y), ncol=2),
+                      test=matrix(c(testing_x, testing_y), ncol=2)
+                      )
+    
+  }
+  return(folds)
+}
+
+
+get_permutation = function(data){
+  #' returns a d dimensional permuted sample 
+  #' @param data description
+  d = dim(data)
+  boot_ind = sample(1:length(data[,1]), length(data[,1]), replace=F)
+  boot_data = matrix(data=NA, nrow=d[1], ncol=d[2])
+  for (i in 1:d[2]){
+    boot_data[, i] = data[,i][boot_ind]
+  }
+  return(boot_data)
+}
+
+
+
